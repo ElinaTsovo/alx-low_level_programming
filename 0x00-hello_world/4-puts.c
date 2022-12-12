3f011f217c37ccfe8fe8532ab795c37a4ed9d949
ElinaTsovo <tsovita1@gmail.com>
2022-12-12 13:17:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *  * main - entry point
  *   *
  *    * Return: 0 after printing the function
- *     */
+      */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 		return (0);
