396ba875b117ced6594691bbc8d3d01d21f3c486
ElinaTsovo <tsovita1@gmail.com>
2022-12-22 13:12:45 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,50 +1,25 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h" 
+char *add_strings(char *n1, char *n2, char *r, int r_index); 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
- * infinite_add - adds two numbers in strings
- * @n1: type string 1
- * @n2: type string 2
- * @r: type buffer string.
- * @size_r: buffer size:
- * Return: pointer dest.
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result. 
+ * @r_index: The current index of the buffer.
+ * Return: If r can store the sum - a pointer to the result.
+ *  If r cannot store the sum - 0. 
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int len1, len2, strg, d2, d3, s3;
-
-	d3 = s3 = 0;
-/* defining length of n1 n2 with bigger */
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-		;
-/* equal the longitude of len to buffer bigger */
-	if (len1 >= len2)
-		strg = c1;
-	if (size_r <= strg)
-		return (0);
-	r [bg + 1]
-	len1 = len1 - 1;
-	len2 = len2 - 1;
-/* defining the longitude in 2 digits to store it in buffer */
-	while (len1 >= 0 || len2 >= 0)
-	{
-		d1 = d2 = 0;
-		if (len1 >= 0)
-			d1 = n1[len1--] - '0';
-		if (len2 >= 0)
-		d2 = n2[len2--] - '0';
-/* sum of d1 and d2 and the carry s3 */
-		d3 = d1 + d2 + s3;
-/* new condition */
-		if (d3 > 9)
-		{
-			s3 = 1;
-			s3 = s3 - 10;
-		}
-		else
-			s3 = 0;
-r[]
-	}
-		return (0);
+	  int num, tens = 0;  
+	  for (; *n1 && *n2; n1--, n2--, r_index--)
+	  {
+		   num = (*n1 - '0') + (*n2 - '0'); 
+		   num += tens;  
+		   *(r + r_index) = (num % 10) + '0';
+		    tens = num / 10;
+	  }
+	   for (; *n1; n1--, r_index--)
 }
+		
